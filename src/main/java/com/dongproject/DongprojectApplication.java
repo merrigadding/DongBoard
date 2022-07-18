d71de3fe7caaceb89c73d8749519ab1407729575
yoodonghyun96 <jaja5280@naver.com>
2022-07-18 23:06:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/dongproject/DongprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dongproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongproject/DongprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
